d51f503e4c2ceb1cc9074d2a83379257a77ba905
oleksandra1musatkina <43454012+oleksandra1musatkina@users.noreply.github.com>
2019-09-09 11:08:20 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <time.h> 
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+int main(void)
+{
+   int i, nahodne_cislo, j;
+   int generovane_cisla[20];
+ 
+   srand( time( 0 ) ); 
+ 
+   printf ("Generovane cisla su:  \n");
+   for (i=1; i<=20; i++)
+   {
+      bool uz_existuje, susedia;
+      do{
+        uz_existuje = false;
+        susedia = false;
+        nahodne_cislo = rand()%20 + 1;
+        if(i>1){
+            for(j=0; j<i-1; j++){
+                if( nahodne_cislo == generovane_cisla[j]){
+                    uz_existuje = true;
+                }
+            }
+            if(generovane_cisla[i-2] == (nahodne_cislo - 1) ||  generovane_cisla[i-2] == (nahodne_cislo + 1)){
+                susedia = true;
+            }
+        }
+      }while(uz_existuje || susedia);
+      generovane_cisla[i-1]=nahodne_cislo;
+      printf ("%d \n",nahodne_cislo);
+
+   }
+ 
+   return 0;
+}
